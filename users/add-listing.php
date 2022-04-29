98ab58bafaee5c348fce256d12a4da4a4b6393ff
Olashina <69033389+olashina201@users.noreply.github.com>
2022-04-29 16:12:44 +0100

    testt

--- users/add-listing.php
+++ users/add-listing.php
@@ -810,9 +810,7 @@ $_SESSION['US_ln'];
 
   <script src="../plugins/jquery/jquery.min.js"></script>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+
   <script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
